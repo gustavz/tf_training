2b74d4dd3110d680ec5b94d4ff762f44aa899edb
GustavZ <gustav.zitzewitz@gmx.de>
2018-05-04 14:08:09 +0200

    Init Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# tf_training
+This repository uses scripts provided by tensorflow's object detection api to train Mask R-CNN with Mobilenet V1 as Backbone on the COCO Dataset.
+<br />
+Additionally: As many local machine have limited computing capacity (especially GPU Memory)
+I added a script that automatically restarts the training and evaluation process from the last saved checkpoint if training got killed by some out of memory / oom or any other error.
+
+## Getting Started
+- inside `dataset/` run `bash download_and_preprocess_mscoco.sh`
+- inside `mask_rcnn_mobilenet_v1_coco.config` modify all absolute paths to fit your directory structure
+- inside `train.sh` modify the path to your `models/research/object_detection` directory of the `TF_DIR` variable
+- run `train.sh`
+- watch the numbers roll!
---
+++ train.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# written and copyright by
+# www.github.com/GustavZ
+
+
+export MODEL="mask_rcnn_mobilenet_v1_coco"
+export TF_DIR="/home/gustav/workspace/tensorflow/tf_models/research/object_detection"
+
+export ROOT_DIR="$(pwd)"
+export CKPT_DIR="${ROOT_DIR}/checkpoints/${MODEL}/train"
+export EVAL_DIR="${ROOT_DIR}/checkpoints/${MODEL}/eval"
+export CFG_FILE=${ROOT_DIR}/${MODEL}.config
+
+echo "> Infinite Tensorflow Training Loop"
+while true; do
+    echo "> update checkpoint"
+    # find old checkpoint
+    old=`sed -n 135p ${CFG_FILE}`
+    old=${old#*"model."}
+    old=${old%\"}
+    echo "> old: ${old}"
+    # find latest checkpoint
+    unset -v latest
+    for file in ${CKPT_DIR}/*".meta"; do
+      [[ $file -nt $latest ]] && latest=$file
+    done
+    latest=${latest%".meta"}
+    latest=${latest#*"model."}
+    echo "> latest: ${latest}"
+    # update config
+    sed -i s/${old}/${latest}/g ${CFG_FILE}
+
+    # Tensorboard % Evaluation
+    echo "> start tensorboard and eval.py in separate terminals with 1m delay"
+    gnome-terminal -x sh -c "sleep 1m;tensorboard --logdir=${ROOT_DIR}"
+    gnome-terminal -x sh -c "sleep 1m;python ${TF_DIR}/eval.py \
+        --logtostderr \
+        --pipeline_config_path=${CFG_FILE} \
+        --checkpoint_dir=${CKPT_DIR} \
+        --eval_dir=${EVAL_DIR}"
+
+    # Start actual training
+    echo "> start training ${MODEL}"
+    python ${TF_DIR}/train.py \
+        --logtostderr  \
+        --pipeline_config_path=${CFG_FILE} \
+        --train_dir=${CKPT_DIR}
+
+    # wait some time and kill remaining processes
+    echo "> waiting 1 minute before restart"
+    sleep 30
+    killall python
+    killall /usr/bin/python
+    sleep 30
+done
